4880849bffc158965a4db8d50dcf043e0a87d657
MIRIAM <miriam@ubuntu.(none)>
2014-12-24 19:02:01 +0100

    añadido codigo practica 6

--- uaclient.py
+++ uaclient.py
@@ -11,23 +11,24 @@ import socket
 
 
 def MensajesLog(mensaje):
-	mensaje = mensaje.split("")
+    mensaje = mensaje.split("")
     fich = open(cHandler.log, "a")
     for cont in mensaje:
         msg = str(time.time()) + " " + str(cont) + " " + "\n"
     fich.write(msg)
     fich.close()
 
+
 class ClientHandler(ContentHandler):
 
-	def __init__(self):
+    def __init__(self):
         self.etiquetas = {
-            "acount": ["username", "passwd"],
+            "account": ["username", "passwd"],
             "uaserver": ["ip", "puerto"],
             "rtpaudio": ["puerto"],
             "regproxy": ["ip", "puerto"],
-			"log": ["path"],
-			"audio": ["path"]}
+            "log": ["path"],
+            "audio": ["path"]}
         self.lista = []
 
     def startElement(self, etiqueta, atributo):
@@ -39,13 +40,69 @@ class ClientHandler(ContentHandler):
 
 
 if __name__ == "__main__":
-	if len(sys.argv) != 4:
+    if len(sys.argv) != 4:
         print("Usage: python uaclient.py config method option")
-		sys.exit()
-	# parseamos el archivo ua1.xml
-	parser = make_parser()
+        sys.exit()
+    # parseamos el archivo ua1.xml
+    parser = make_parser()
     cHandler = ClientHandler()
     parser.setContentHandler(cHandler)
     parser.parse(open(sys.argv[1]))
+
+	# cogemos los parametros pasados por argumento
+	METODO = sys.argv[2]
+	OPCION = sys.argv[3]
+
+	# Dirección IP del servidor y métodos
+	IP = cHandler.regproxy_ip
+	PORT = cHandler.regproxy_puerto
+	METODO = ["REGISTER", "INVITE", "BYE"]
+
+	# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+	my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	my_socket.connect((IP, PORT))
+
+	LINE, LINE2 = ""
+	#creamos LINE segun el tipo del metodo que pasamos como argumento
+	if METODO == "REGISTER":
+		LINE = "REGISTER sip:" + cHandler.account_username + " SIP/2.0" + "\r\n"
+		LINE2 = "Expires: " + OPCION + "\r\n"
+	elif METODO == "INVITE":
+		LINE = "INVITE sip:" + OPCION + " SIP/2.0" + '\r\n'
+		LINE2 = "v=0" + "\n" + "o=" + cHandler.account_username + " " + IP + 
+				"\n" + "s=misesion" + "\n" + "t=0" + "\n" + "m=audio " +
+				cHandler.rtpaudio_puerto + " RTP"
+	elif METODO == "BYE":
+		LINE = "BYE sip:" + OPCION + " SIP/2.0" + '\r\n'
+
+	print "Enviando: " + LINE
+	my_socket.send(LINE + '\r\n')
+
+	try:
+		data = my_socket.recv(1024)
+		print 'Recibido -- ', data
+	except socket.error:
+		sys.exit("Error: No server listening at " + IP + " port " + str(PORT))
+
+	if METODO == "REGISTER":	
+	
+	elif METODO == "INVITE":
+		datos = data.split(" ")
+		if data == ("SIP/2.0 100 Trying" + '\r\n\r\n' + "SIP/2.0 180 Ringing" +
+		            '\r\n\r\n' + "SIP/2.0 200 OK" + '\r\n\r\n'):
+		    LINE = "ACK sip:" + LOGIN + "@" + IP + " SIP/2.0"
+		    print "Enviando: " + LINE
+		    my_socket.send(LINE + '\r\n\r\n')
+		    data = my_socket.recv(1024)
+		    print 'Recibido -- ', data
+
+	elif METODO == "BYE":
+
+	print "Terminando socket..."
+
+	# Cerramos todo
+	my_socket.close()
+	print "Fin."
 
 
