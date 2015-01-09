c275d329caf92016f1d1df7cfd90dfb464d2b0f3
MIRIAM <miriam@ubuntu.(none)>
2015-01-09 20:48:37 +0100

    terminado

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -8,14 +8,12 @@ import sys
 import os
 import time
 import socket
-# ejecutamos el proxy: python proxy_registrar.py pr.xml
 
 
 def MensajesLog(mensaje):
-    mensaje = mensaje.split("")
-    fich = open(cHandler.log_path, "a")
-    for cont in mensaje:
-        msg = str(time.time()) + " " + str(cont) + " " + "\n"
+    mensaje = mensaje.split(" ")
+    fich = open(cHandler.log, "a")
+    msg = str(time.time()) + " " + str(mensaje) + " " + "\n"
     fich.write(msg)
     fich.close()
 
@@ -23,95 +21,102 @@ def MensajesLog(mensaje):
 class ProxyHandler(ContentHandler):
 
     def __init__(self):
-        self.etiquetas = {
-            "server": ["name", "ip", "puerto"],
-            "database": ["path", "passwdpath"],
-            "log": ["path"],
-        self.lista = []
+        self.name = ""
+        self.ip = ""
+        self.puerto = ""
+        self.database_path = ""
+        self.database_passwdpath = ""
+        self.log = ""
 
     def startElement(self, etiqueta, atributo):
-        if etiqueta in self.etiquetas:
-            dic = {}
-            for attr in self.etiquetas[etiqueta]:
-                dic[attr] = (atributo.get(attr, ""))
-            self.lista.append([etiqueta, dic])
+        if etiqueta == "server":
+            self.server_name = atributo.get("username", "")
+            self.server_ip = atributo.get("ip", "127.0.0.1")
+            self.server_puerto = atributo.get("puerto", "")
+        elif etiqueta == "database":
+            self.database_path = atributo.get("path", "")
+            self.passwd_path = atributo.get("passwdpath", "")
+        elif etiqueta == "log":
+            self.log = atributo.get("path", "")
 
-    dicc = {}
+dicc = {}
 
 
 class ProxyRegistrar(SocketServer.DatagramRequestHandler):
 
+    dicc = {}
+
     def register2file(self):
-            fich = open(cHandler.database_path, "a")
-            fich.write("User\tIP\tPuerto\tExpires\n")
-            for cliente in self.dicc.items():
-                hora = time.strftime('%Y-­%m-­%d %H:%M:%S',
-                             time.gmtime(time.time()))
-                fich.write(cliente + '\t' + IP + '\t' +  PUERTO + '\t' + hora +
-                '\n')
-            fich.close()
+        fich = open(cHandler.database_path, "a")
+        fich.write("User\tIP\tPuerto\tExpires\n")
+        for cliente, valor in self.dicc.items():
+            ip = valor.split(",")[0]
+            puerto = valor.split(",")[1]
+            hora = time.strftime('%Y-­%m-­%d %H:%M:%S',
+                                 time.gmtime(time.time()))
+            fich.write(cliente + '\t' + ip + '\t' + puerto + '\t' + hora +
+                       '\n')
+        fich.close()
 
     def handle(self):
         line = self.rfile.read()
         line2 = line.split(" ")
-        line3 = line2.split(":")
+        line3 = line.split(":")
         IP = str(self.client_address[0])
         cliente = line2[1].split(":")[1]
-        EXPIRES = line3[3]
-        tiempo = time.time() + int(EXPIRES)
+        self.dicc[cliente] = IP
         hora_actual = time.time()
 
-"""
-2. ip_server y puerto_server los obtienes de un registro anterior del receptor
-del INVITE. Si no está registrado, hay que devolver un 404.
-"""
         print "El cliente nos manda " + line
-
         if line2[0] == "REGISTER":
             PUERTO = str(line2[1].split(":")[2])
+            EXPIRES = line3[3]
+            tiempo = time.time() + int(EXPIRES)
             registrado = "Received from " + IP + ":" + PUERTO + ": REGISTER"
             MensajesLog(registrado)
             self.wfile.write("SIP/2.0 200 OK" + '\r\n\r\n')
-            enviado = "Sent to " + IP + ":" + PUERTO ": 200 OK"
+            enviado = "Sent to " + IP + ":" + PUERTO + ": 200 OK"
             MensajesLog(enviado)
             if EXPIRES == 0:
                 if cliente in self.dicc:
                     # borramos al usuario
                     del self.dicc[cliente]
-                    self.register2file(cliente)
+                    self.register2file()
             else:
-                self.dicc[cliente] = IP + ", " + str(tiempo)
-                self.register2file(cliente)
+                self.dicc[cliente] = IP + ", " + PUERTO + ", " + str(tiempo)
+                self.register2file()
 
             for elemento, valor in self.dicc.items():
                 hora = valor.split(",")[-1]
                 if hora_actual > hora:
                     del self.dicc[elemento]
-                    self.register2file(cliente)
+                    self.register2file()
 
         elif line2[0] == "INVITE":
             encontrado = False
-            for i in dicc:
+            for i in self.dicc:
                 if cliente == i:
                     encontrado = True
 
             if encontrado:
-                recibido = "Received from " + IP + ":" + PUERTO + ": INVITE"
+                recibido = ("Received from " + IP + ":" +
+                            self.dicc[cliente][1] + ": INVITE")
                 MensajesLog(recibido)
                 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
                 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                my_socket.connect((IP_DESTINO, PUERTO_DESTINO))
+                my_socket.connect((IP, int(self.dicc[cliente][1])))
                 my_socket.send(line)
-                enviado = "Sent to " + IP_DESTINO + ":" + PUERTO_DESTINO +
-                            ": INVITE"
+                enviado = ("Sent to " + IP + ":" + self.dicc[cliente][1] +
+                           ": INVITE")
                 MensajesLog(enviado)
 
                 try:
                     data = my_socket.recv(1024)
                     print 'Recibido -- ', data
                 except socket.error:
-                    error = "Error: No server listening at " +
-                            cHandler.server_ip + " port " + server_puerto
+                    error = ("Error: No server listening at " +
+                             cHandler.server_ip + " port " +
+                             cHandler.server_puerto)
                     sys.exit(error)
                     MensajesLog(error)
                     my_socket.close()
@@ -120,8 +125,8 @@ del INVITE. Si no está registrado, hay que devolver un 404.
                     print "Fin."
 
                 print 'Recibido -- ', data
-                reenviado = "Resent to " + IP_DESTINO + ":" + PUERTO_DESTINO
-                            + ": " + data
+                reenviado = ("Resent to " + IP + ":" + self.dicc[cliente][1] +
+                             ": " + data)
                 MensajesLog(reenviado)
                 print 'Enviando -- ', data
                 self.wfile.write(data)
@@ -130,33 +135,34 @@ del INVITE. Si no está registrado, hay que devolver un 404.
                 self.wfile.write("SIP/2.0 404 User Not Found" + '\r\n\r\n')
 
         elif line2[0] == "ACK":
-            recibido = "Received from " + IP + ":" + PUERTO + ": ACK"
+            recibido = ("Received from " + IP + ":" + self.dicc[cliente][1] +
+                        ": ACK")
             MensajesLog(recibido)
             my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            my_socket.connect((IP_DESTINO, PUERTO_DESTINO))
+            my_socket.connect((IP, self.dicc[cliente][1]))
             my_socket.send(line)
-            enviado = "Sent to " + IP_DESTINO + ":" + PUERTO_DESTINO +
-                        ": ACK"
+            enviado = ("Sent to " + IP + ":" + self.dicc[cliente][1] + ": ACK")
             MensajesLog(enviado)
 
         elif line2[0] == "BYE":
-            recibido = "Received from " + IP + ":" + PUERTO + ": BYE"
+            recibido = ("Received from " + IP + ":" + self.dicc[cliente][1] +
+                        ": BYE")
             MensajesLog(recibido)
             my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            my_socket.connect((IP_DESTINO, PUERTO_DESTINO))
+            my_socket.connect((IP, self.dicc[cliente][1]))
             my_socket.send(line)
-            enviado = "Sent to " + IP_DESTINO + ":" + PUERTO_DESTINO +
-                        ": BYE"
+            enviado = ("Sent to " + IP + ":" + self.dicc[cliente][1] + ": BYE")
             MensajesLog(enviado)
 
             try:
                 data = my_socket.recv(1024)
                 print 'Recibido -- ', data
             except socket.error:
-                error = "Error: No server listening at " +
-                            cHandler.server_ip + " port " + server_puerto
+                error = ("Error: No server listening at " +
+                         cHandler.server_ip + " port " +
+                         cHandler.server_puerto)
                 sys.exit(error)
                 MensajesLog(error)
                 my_socket.close()
@@ -165,8 +171,8 @@ del INVITE. Si no está registrado, hay que devolver un 404.
                 print "Fin."
 
             print 'Recibido -- ', data
-            reenviado = "Resent to " + IP_DESTINO + ":" + PUERTO_DESTINO +
-                        ": " + data
+            reenviado = ("Resent to " + IP + ":" + self.dicc[cliente][1] +
+                         ": " + data)
             MensajesLog(reenviado)
             print 'Enviando -- ', data
             self.wfile.write(data)
@@ -176,8 +182,9 @@ del INVITE. Si no está registrado, hay que devolver un 404.
                 data = my_socket.recv(1024)
                 print 'Recibido -- ', data
             except socket.error:
-                error = "Error: No server listening at " +
-                            cHandler.server_ip + " port " + server_puerto
+                error = ("Error: No server listening at " +
+                         cHandler.server_ip + " port " +
+                         cHandler.server_puerto)
                 sys.exit(error)
                 MensajesLog(error)
                 my_socket.close()
@@ -186,12 +193,12 @@ del INVITE. Si no está registrado, hay que devolver un 404.
                 print "Fin."
 
             print 'Recibido -- ', data
-            reenviado = "Resent to " + IP_DESTINO + ":" + PUERTO_DESTINO +
-                        ": " + data
+            reenviado = ("Resent to " + IP_ + ":" + self.dicc[cliente][1] +
+                         ": " + data)
             MensajesLog(reenviado)
             my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            my_socket.connect((IP_DESTINO, PUERTO_DESTINO))
+            my_socket.connect((IP, self.dicc[cliente][1]))
             my_socket.send(data)
             print 'Enviando -- ', data
             self.wfile.write(data)
@@ -209,15 +216,15 @@ if __name__ == "__main__":
 
     # parseamos el archivo ua2.xml
     parser = make_parser()
-    cHandler = ProxyRegistrar()
+    cHandler = ProxyHandler()
     parser.setContentHandler(cHandler)
     parser.parse(open(sys.argv[1]))
-
     # Creamos servidor de eco y escuchamos
     proxy = SocketServer.UDPServer((str(cHandler.server_ip),
-            int(cHandler.server_puerto)), ProxyRegistrar)
-    servidor = "Server MiServidorBigBang listening at port " +
-                cHandler.server_puerto + "..."
+                                    int(cHandler.server_puerto)),
+                                   ProxyRegistrar)
+    servidor = ("Server MiServidorBigBang listening at port " +
+                cHandler.server_puerto + "...")
     print servidor
     proxy.serve_forever()
     MensajesLog(servidor)
