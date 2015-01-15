698b5e4df85921a5eff9a2262f373da5ffff014a
miriammz <miri_mz@hotmail.com>
2015-01-15 14:13:26 +0100

    Update uaserver.py
    líneas modificadas, pep8 pasado después de la corrección de grupo

--- uaserver.py
+++ uaserver.py
@@ -74,10 +74,10 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                      " " + IP + '\n' + "s=misesion" + '\n' + "t=0" + '\n' +
                      "m=audio " + cHandler.rtpaudio_puerto + " RTP")
             MensajesLog(envio)
-            SDP = ("Content-Type: application/sdp" + '\r\n\r\n' + "v=0" + '\r\n' +
-                   "o=" + cHandler.account_username + " " + IP + '\r\n' +
-                   "s=misesion" + '\r\n' + "t=0" + '\r\n' + "m=audio " +
-                   cHandler.rtpaudio_puerto + " RTP")
+            SDP = ("Content-Type: application/sdp" + '\r\n\r\n' + "v=0" +
+                   '\r\n' + "o=" + cHandler.account_username + " " + IP +
+                   '\r\n' + "s=misesion" + '\r\n' + "t=0" + '\r\n' +
+                   "m=audio " + cHandler.rtpaudio_puerto + " RTP")
             self.wfile.write("SIP/2.0 100 Trying" + '\r\n\r\n' +
                              "SIP/2.0 180 Ringing" + '\r\n\r\n' +
                              "SIP/2.0 200 OK" + '\r\n\r\n' + SDP)
